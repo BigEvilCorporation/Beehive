9c45f2695e40f476f793ae86318108c70b47c29d
Matt <mattphillips@mail.com>
2016-04-24 21:51:52 +0100

    Beehive: WIP sprite import

---
+++ mdstudio/Sprite.cpp
@@ -0,0 +1,75 @@
+///////////////////////////////////////////////////////
+// Beehive: A complete SEGA Mega Drive content tool
+//
+// (c) 2016 Matt Phillips, Big Evil Corporation
+///////////////////////////////////////////////////////
+
+#include <ion/core/Debug.h>
+
+#include "Sprite.h"
+
+Sprite::Sprite()
+{
+
+}
+
+void Sprite::ImportBitmap(const std::string& filename, int widthFrames, int heightFrames, int maxFrames)
+{
+	//Read BMP
+	BMPReader reader;
+	if(reader.Read(filename))
+	{
+		if(reader.GetWidth() % 8 != 0 || reader.GetHeight() % 8 != 0)
+		{
+			if(wxMessageBox("Bitmap width/height is not multiple of 8, image will be truncated", "Warning", wxOK | wxCANCEL | wxICON_WARNING) == wxCANCEL)
+			{
+				return false;
+			}
+		}
+
+		//Get width/height in tiles
+		int widthTiles = reader.GetWidth() / 8;
+		int heightTiles = reader.GetHeight() / 8;
+
+		//Get frame width/height in tiles
+		int frameWidthTiles = widthTiles / widthFrame;
+		int frameHeightTiles = heightTiles / heightFrame;
+
+		//For each frame
+		for(int frameY = 0; frameY < heightframes; frameY++)
+		{
+			for(int frameX = 0; frameX < widthframes; frameX++)
+			{
+				//Create new frame
+				SpriteFrame frame;
+
+				//For each tile in frame (Mega Drive order = column major)
+				for(int tileX = 0; tileX < frameWidthTiles; tileX++)
+				{
+					for(int tileY = 0; tileY < frameHeightTiles; tileY++)
+					{
+						//Read tile
+						Tile tile;
+
+					}
+				}
+			}
+		}
+	}
+}
+
+const SpriteFrame& Sprite::GetFrame(int index) const
+{
+	ion::debug::Assert(index >= 0 && index < GetNumFrames(), "Sprite::GetFrame() - Out of range");
+	return m_frames[index];
+}
+
+int Sprite::GetNumFrames() const
+{
+	return m_frames.size();
+}
+
+const Palette& Sprite::GetPalette() const
+{
+	return m_palette;
+}
---
+++ mdstudio/Sprite.h
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////
+// Beehive: A complete SEGA Mega Drive content tool
+//
+// (c) 2016 Matt Phillips, Big Evil Corporation
+///////////////////////////////////////////////////////
+
+#pragma once
+
+#include "Tile.h"
+#include "Palette.h"
+
+typedef std::vector<Tile> SpriteFrame;
+
+class Sprite
+{
+public:
+	Sprite();
+
+	void ImportBitmap(const std::string& filename, int widthCells, int heightCells, int maxCells);
+
+	const SpriteFrame& GetFrame(int index) const;
+	int GetNumFrames() const;
+
+	const Palette& GetPalette() const;
+
+private:
+	std::vector<SpriteFrame> m_frames;
+	Palette m_palette;
+};
