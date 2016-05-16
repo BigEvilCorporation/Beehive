62927b36d0579a62ae2262cf130591e317b9fbad
Matt <mattphillips@mail.com>
2016-05-16 22:19:33 +0100

    Missing files

---
+++ mdstudio/Types.h
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////
+// Beehive: A complete SEGA Mega Drive content tool
+//
+// (c) 2016 Matt Phillips, Big Evil Corporation
+///////////////////////////////////////////////////////
+
+#pragma once
+
+#include <ion/core/cryptography/UUID.h>
+
+typedef u32 GameObjectTypeId;
+typedef u32 GameObjectId;
+typedef ion::UUID64 AnimationId;
+typedef ion::UUID64 SpriteAnimId;
+
+static const GameObjectTypeId InvalidGameObjectTypeId = 0;
+static const GameObjectTypeId InvalidGameObjectId = 0;
+static const AnimationId InvalidAnimationId = 0;
+static const SpriteAnimId InvalidSpriteAnimId = 0;
