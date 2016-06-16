1dd8cdaac63e4fc6ec30a0fb6494e29248219c73
Matt <mattphillips@mail.com>
2016-06-16 20:32:39 +0100

    Removed ion::engine

--- ion/test/iotest/GenerateWorkspace_VS2013.bat
+++
@@ -1,3 +0,0 @@
-..\..\..\BuildTools\JamPlus\Bin\Win32\Jam.exe --workspace --gen=vs2013 iotest.jam _Build
-
-CALL _Build\_workspace.vs2013_\IOTest.sln
--- ion/test/physicstest/GenerateWorkspace_VS2010.bat
+++
@@ -1,3 +0,0 @@
-..\..\..\BuildTools\JamPlus\Bin\Win32\Jam.exe --workspace --gen=vs2010 physicstest.jam _Build
-
-CALL _Build\_workspace.vs2010_\PhysicsTest.sln
