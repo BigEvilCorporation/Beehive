6ebc69a6ec5cdb0cea8015bbb4307c10bc244f29
Matt <mattphillips@mail.com>
2017-01-23 20:39:38 +0000

    Update to new Jamplus

--- mdstudio/GenerateWorkspace_VS2013.bat
+++ mdstudio/GenerateWorkspace_VS2013.bat
@@ -1,3 +1,3 @@
-..\BuildTools\JamPlus\Bin\Win32\Jam.exe --workspace --gen=vs2013 --config=jamplus.config beehivestudio.jam _Build
+..\BuildTools\JamPlus\Bin\Win64\Jam.exe --workspace --gen=vs2013 --config=jamplus.config beehivestudio.jam _Build
 
-CALL _Build\_workspace.vs2013_\beehivestudio.sln
+CALL _Build\_workspaces_\vs2013\beehivestudio.sln
