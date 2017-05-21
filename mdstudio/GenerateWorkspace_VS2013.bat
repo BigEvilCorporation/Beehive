af9cebb31e0c1f9f18997395ea1c3cb4c86a2092
Matt <mattphillips@mail.com>
2017-05-21 11:46:39 +0100

    Fixed project generation for VS2013

--- mdstudio/GenerateWorkspace_VS2013.bat
+++ mdstudio/GenerateWorkspace_VS2013.bat
@@ -1,3 +1,3 @@
-..\BuildTools\JamPlus\Bin\Win64\Jam.exe --workspace --gen=vs2013 --config=jamplus.config beehivestudio.jam _Build
+..\BuildTools\JamPlus\Bin\Win64\Jam.exe --workspace --gen=vs2013 -compiler=vs2013 --config=jamplus.config beehivestudio.jam _Build
 
 CALL _Build\_workspaces_\vs2013\beehivestudio.sln
