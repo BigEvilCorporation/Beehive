fa413644393b5aa65fca5641df9153cbc801c0ad
DESKTOP-99MT4GR\Matt <matt@tanglewoodgame.com>
2018-05-17 19:13:05 +0100

    VS2017 .bat

---
+++ mdstudio/GenerateWorkspace_VS2017.bat
@@ -0,0 +1,3 @@
+..\BuildTools\JamPlus\Bin\Win64\Jam.exe --workspace --gen=vs2017 -compiler=vs2017 --config=jamplus.config beehivestudio.jam _Build
+
+CALL _Build\_workspaces_\vs2017\beehivestudio.sln
