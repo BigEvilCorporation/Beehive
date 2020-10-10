3818882b1cef96c32b0346566a0a47fe0b0add5c
Matt Phillips <mattphillips@mail.com>
2020-10-11 00:25:49 +0100

    VS2019 workspace

---
+++ mdstudio/GenerateWorkspace_VS2019.bat
@@ -0,0 +1,3 @@
+..\BuildTools\JamPlus\Bin\Win64\Jam.exe --workspace --gen=vs2019 -compiler=vs2019 --config=jamplus.config beehivestudio.jam _Build
+
+CALL _Build\_workspaces_\vs2019\beehivestudio.sln
